4a0b0a50aa0140a4832a50428e2f4f006301557c
satan-1412 <62664082+satan-1412@users.noreply.github.com>
2026-02-24 01:52:29 +0800

    Update DynamicGamepadView.java

--- app/src/main/java/org/libsdl/app/DynamicGamepadView.java
+++ app/src/main/java/org/libsdl/app/DynamicGamepadView.java
@@ -17,6 +17,7 @@ import android.view.HapticFeedbackConstants;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
