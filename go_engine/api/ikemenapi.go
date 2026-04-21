5120fa691e646554790c3f07dd163aa472c2b584
satan-1412 <62664082+satan-1412@users.noreply.github.com>
2026-04-21 17:54:32 +0800

    Add files via upload

--- go_engine/api/ikemenapi.go
+++ go_engine/api/ikemenapi.go
@@ -2,19 +2,14 @@ package api
 
 import (
 	"bytes"
-	"encoding/binary"
-	"errors"
-	"fmt"
+	"encoding/json"
 	"image"
-	"image/color"
-	"image/draw" // 🔥 补上了渲染需要的 draw 包
-	_ "image/gif"
-	_ "image/jpeg"
+	"image/draw"
+	"image/gif"
 	"image/png"
-	"io"
-	// 🔥 删除了这里原本没有用到的 "math"
 	"os"
-)
+	"path/filepath"
+	"sort"
 
 	// 导入我们的两大核心提纯车间
 	"ikemenbridge/sff_module"
